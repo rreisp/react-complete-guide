1343a5710ba922c3c1245ef958849d4b1fa1625b
Rodrigo Reis <rreisp@gmail.com>
2022-12-12 09:24:54 -0300

    Time to Practice: Working with Lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,19 +8,19 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = (selectedYear) => {
+  const filterChangeHanlder = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        {props.items.map((expense) => (
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHanlder} />
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
